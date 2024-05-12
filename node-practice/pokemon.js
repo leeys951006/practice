b880937c6699cc35dc9b010f6c1624c3f21a7f04
leeys951006 <168498691+leeys951006@users.noreply.github.com>
2024-05-12 22:43:19 +0900

    버튼 클릭시 포켓몬 나오게하는 연습

--- node-practice/pokemon.js
+++ node-practice/pokemon.js
@@ -6,37 +6,26 @@ let url = "http://localhost:3000";
 let root = document.getElementById('root')
 let pokemonimage = document.createElement('h1')
 root.appendChild(pokemonimage)
-let selecbutton = document.createElement('div')
+let selecbutton = document.createElement('button')
 root.appendChild(selecbutton)
+let selecword = document.createTextNode('당신의 포켓몬은 뭘까~요? 피피카츄~!')
+selecbutton.appendChild(selecword)
 
 
-//* 클라이언트에서 서버로 요청하는 방식은 2가지가 있습니다.
-//* XHTTPRequest, fecth
-//* fetch를 이용해서 해볼게요
 
-//* 데이터를 요청하고 처리하려면 비동기 방식을 알아야 해요.
-//* async를 붙여 비동기제어를 한다고 알려줍시다.
-//! async, await 방식
-let request = async function(){
-  //* 비동기 쓰는 방식이 2가지가 있습니다. 
-  //* (1) async, await (2) then 
-  //* 2가지다 보여드릴게요. 
+// //! async, await 방식
+// let request = async function(){
 
-  //* await를 만나면 이 함수를 호출한 상위 환경으로 넘어가게 됩니다. 
+//   const res = await fetch(url + "/data.json", {method : "GET"});
 
-  //* await를 만나면 해당 지점에서 멈추게 됩니다.
-  //* (1) await를 만나면 fetch로 데이터를 받을 때 까지 대기 해라는 뜻입니다.
-  //! 중요 "/data.json"를 한 이유는 서버에서 req.url로 분류하기 위함입니다.
-  const res = await fetch(url + "/data.json", {method : "GET"});
-  //* (2) await를 만나면 해당 data를 객체로 파싱할때까지 대기해라는 뜻입니다.
-  const data = await res.json();
+//   const data = await res.json();
 
 
-  const getData = pokemonSelector(data);
-  let PText = document.createTextNode(getData);
-  pokemonimage.appendChild(PText)
+//   const getData = pokemonSelector(data);
+//   let PText = document.createTextNode(getData);
+//   pokemonimage.appendChild(PText)
   
-}
+// }
 
 
 //! then, Promise 방식?
@@ -44,24 +33,19 @@ let requestL1 = fetch(url + "/data.json", {
   method : "GET"
 }
 )
-//* 콜백함수입니다. fetch를 통해 데이터가 들어오면, then으로 넘어갑니다. 
+
 .then(res => res.json())
-//* 객체로 파싱된 data는 jsonData 입니다
+
 .then(data => {
   //TODO 객체로 변환된 JSON 데이터를 이용해주시면되요.
   //* 예시
   const getData = pokemonSelector(data);
   let PText = document.createTextNode(getData);
-  pokemonimage.appendChild(PText)
+  pokemonimage.appendChild(PText);
+
 }); 
 
-
-
-
-
-
-
-function pokemonSelector(array) {
+function pokemonSelector(data) {
   
   function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
@@ -69,18 +53,51 @@ function pokemonSelector(array) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
   }
 
-  const firstGetRandomIndex = getRandomInt(0, array.length);
+  const firstGetRandomIndex = getRandomInt(0, data.length);
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     
-    
-    if (Array.isArray(array[firstGetRandomIndex]) === true) {
-      const randomIndex = getRandomInt(0, array[i].length);
-
-      for (let j = 0; j < array[i].length; j++) {
-        return array[firstGetRandomIndex][randomIndex];
-      }
+    if (Array.isArray(data[firstGetRandomIndex]) === true) {
+     
     }
   }
-  return array[firstGetRandomIndex];
+  return data[firstGetRandomIndex];
 }
+
+
+selecbutton.addEventListener('click',function pokemonSelector(data){
+});
+
+
+
+
+
+
+
+
+
+// function pokemonSelector(data) {
+  
+//   function getRandomInt(min, max) {
+//     const minCeiled = Math.ceil(min);
+//     const maxFloored = Math.floor(max);
+//     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+//   }
+
+//   const firstGetRandomIndex = getRandomInt(0, data.length);
+
+//   for (let i = 0; i < data.length; i++) {
+    
+    
+//     if (Array.isArray(data[firstGetRandomIndex]) === true) {
+//       const randomIndex = getRandomInt(0, data[i].length);
+
+//       for (let j = 0; j < data[i].length; j++) {
+//         return data[firstGetRandomIndex][randomIndex];
+//       }
+//     }
+//   }
+//   return data[firstGetRandomIndex];
+// }
+
+// selecbutton.addEventListener("click",function(){})
--- node-practice/server-1.js
+++ node-practice/server-1.js
@@ -18,61 +18,36 @@ let html =
 `;
 
 
-//* 이거는 파일을 만드는 메소드
 fs.writeFile('pokemon.html', html, function (){});
 
-//* 서버가 있어야 해당 파일을 보내줄수 있어요.
-//* 서버를 만드는 함수에요.
 http.createServer((req, res) => {
 
-  //* 브라우저에서 해당 서버를 접속할 때 파일들을 요청해요.
-  //* req => request.
- 
-  //* 처음요청할때 "/"로 브라우저가 자동으로 요청해요
-  //* 좀있다 다른 화면으로 보여드릴게요.
-  //* 이 때 Html파일을 보내주면 되요.
   if(req.url == "/"){
-    //* 이제 우리는 파일을 읽어서 브라우저한테 보내줘야 합니다.
 
-    //* 파일 위치
     let path = "pokemon.html"
 
-    //* 매개변수로, 파일 위치와 콜백함수가 필요해요.
     fs.readFile(path, (err,data) => {
 
-      //* 파일을 읽지 못해서 에러날 떄. 해당 조건문으로 이동합니다.
-      //* res는 response의 줄임말이에요. 응답을 뜻합니다.
       if(err){
         res.writeHead(500);
         res.end("File Error");
         return
       }
 
-      //* 에러가 나지 않았을 경우 
       else{
-        //* 여기서 숫자 200은 응답이 잘 됬다는 뜻입니다.
-        //* mdn statuscode 검색하시면 나오실거에요.
-        //* content-type은 해당 파일이 html이라고 알려주는 거에요.
+
         res.writeHead(200, {'Content-Type' : 'text/html'})
 
-        //* data는 path위치에 있는 html파일이라고 생각하시면 되요.
         res.end(data);
       }
     })
   }
-    //* 두 번쨰로는 조건문으로 js파일에 대한 요청을 응답해야합니다.
-    //* 브라우저가 html을 읽을 떄, 위에서 아래로 읽게 될텐데.
-    //* 브라우저는 pokemon-1.js 파일을 가지고 있지 않기 때문에
-    //* 해당 <script> 항목을 만나면 서버에거 해당 js파일을 요청하게 되요.
 
-  //* 브라우저로 부터의 해당 js요청.
   if(req.url == "/pokemon.js"){
 
       let path = "pokemon.js";
 
-      //* 위에서 똑같이 파일을 읽어서 응답해 줍시다.
       fs.readFile(path, (err, data) => {
-        //* 에러처리
         if(err){
           res.writeHead(500);
           res.end("File Error");
@@ -80,20 +55,19 @@ http.createServer((req, res) => {
         }
 
         else{
-          //* 해당 파일이 자바스크립트라고 명시해줍시다.
-          //* 명시를 안해주면 브라우저는 모릅니다.
+
           res.writeHead(200, {'Content-Type' : 'application/javascript'})
           res.end(data);
         }
       })
     }
   
-  //* 아까 /data.json을 붙여주었기 때문에 해당 조건문으로 넘어가게 됩니다.
+
   if(req.url == "/data.json")
   {
     let path = "pokemonNames.json";
 
-    //* 똑같이 파일을 읽어 클라이언트에게 넘겨줍시다.
+
     fs.readFile(path, (err,data) => {
       if(err){
         res.writeHead(500);
